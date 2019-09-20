12cb4e5af8dbdd4dbda137c3477a101a849a1a3e
hadywalied <my.mepharo1230@gmail.com>
2019-09-20 22:15:49 +0200

    fixed magazine model

--- app/src/main/java/com/kVectorFoundation/KV/Model/MagList.kt
+++ app/src/main/java/com/kVectorFoundation/KV/Model/MagList.kt
@@ -3,8 +3,7 @@ package com.kVectorFoundation.KV.Model
 import androidx.lifecycle.MutableLiveData
 import com.squareup.moshi.Json
 
-class MagList{
+data class MagList(
     @field:Json(name = "data")
-    var data: MutableLiveData<Model.MagModel>?=null
-
-}
+    var data: List<MagList>
+)
